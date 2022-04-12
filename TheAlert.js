d02c05f329bc86fbb91b98c39e5e0a78ea101c2a
郭家秀 <a880318@gmail.com>
2022-04-12 11:12:04 +0800

    add btn in alertName component

--- TheAlert.js
+++ TheAlert.js
@@ -1,4 +1,9 @@
 export default{
     props:['Name'],
-    template: /*HTML*/ '<li>{{Name}}</li>'
+    template: /*HTML*/ '<li>{{Name}} <button v-on:click="alertName">click me</button></li>',
+    methods:{
+        alertName(){
+            alert(this.Name)
+        }
+    }
 }

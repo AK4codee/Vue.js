3385488f8103b23595f58bcacb6936cb3003a872
郭家秀 <a880318@gmail.com>
2022-04-12 11:21:30 +0800

    emit: child call parent's method

--- TheAlert.js
+++ TheAlert.js
@@ -3,7 +3,8 @@ export default{
     template: /*HTML*/ '<li>{{Name}} <button v-on:click="alertName">click me</button></li>',
     methods:{
         alertName(){
-            alert(this.Name)
+            //alert(this.Name)
+            this.$emit('clickme2')
         }
     }
 }
